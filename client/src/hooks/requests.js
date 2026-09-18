2e1c305bc81feceb942bf7b39fc77a3726d7fa83
abdulkousara-droid <abdulkader.kousara@gmail.com>
2026-09-19 00:32:51 +0200

    added launches post and delete

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -16,18 +16,33 @@ async function httpGetLaunches() {
 
   // Submit given launch data to launch system.
 async function httpSubmitLaunch(launch) {
-  await fetch(`${API_URL}/planets`, {
-    method: 'POST',
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(launch)
-  })
+  try {
+    return await fetch(`${API_URL}/launches`, {
+      method: 'post',
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(launch)
+    })
+  }catch(err) {
+    return {
+      ok: false,
+    }
+  }
 }
-
+ // TODO: Once API is ready.
 async function httpAbortLaunch(id) {
-  // TODO: Once API is ready.
-  // Delete launch with given ID.
+  try {
+    return await fetch(`${API_URL}/launches/${id}`, {
+      method: 'delete',
+    });
+  }catch(err) {
+    console.log(err)
+    return {
+      ok: false,
+    }
+  }
+
 }
 
 export {
--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -1,4 +1,4 @@
-const { getAllLaunches, addNewLaunch} = require('../../models/launches.model');
+const { getAllLaunches, addNewLaunch, existsLaunchWithId, abortLaunchById} = require('../../models/launches.model');
 
 function httpGetAllLaunches(req, res) {
     return res.status(200).json(getAllLaunches());
@@ -7,7 +7,7 @@ function httpGetAllLaunches(req, res) {
 function httpAddNewLaunch(req, res) {
     const launch = req.body;
 
-    if(!launch.mission || !launch.rocket || !launch.launchDate || !launch.destination) {
+    if(!launch.mission || !launch.rocket || !launch.launchDate || !launch.target) {
         return res.status(400).json({
             error: 'Missing required launch property',
         })
@@ -25,7 +25,26 @@ function httpAddNewLaunch(req, res) {
 
 }
 
+function httpAbortLaunch(req, res) {
+    const launchId = Number(req.params.id);
+
+    //if launch doesn't exist
+    if(!existsLaunchWithId(launchId)){
+        return res.status(404).json({
+            error: 'Launch not found',
+        })
+    }
+
+
+    //if launch does exist
+    const aborted = abortLaunchById(launchId);
+    return res.status(200).json(aborted)
+
+
+}
+
 module.exports = {
     httpGetAllLaunches,
     httpAddNewLaunch,
+    httpAbortLaunch,
 };
